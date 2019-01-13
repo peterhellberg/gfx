6980c0455cb3f9286a0d974c582c073099311b64
Peter Hellberg <peter@c7.se>
2019-01-13 02:53:04 +0100

    Initial drawing functions

---
+++ draw.go
@@ -0,0 +1,120 @@
+package gfx
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+)
+
+// Drawer is the draw interface.
+type Drawer interface {
+	Draw(draw.Image)
+}
+
+// DrawLine draws a line of the given color.
+// A thickness of <= 1 is drawn using DrawBresenhamLine.
+func DrawLine(m draw.Image, from, to Vec, t float64, c color.Color) {
+	if t <= 1 {
+		DrawBresenhamLine(m, from, to, c)
+	}
+
+	polylineFromTo(from, to, t).Fill(m, c)
+}
+
+// DrawFilledCircle draws a (crude) filled circle for now.
+func DrawFilledCircle(m draw.Image, u Vec, c color.Color, r float64) {
+	ir := int(r)
+	r2 := ir * ir
+	pt := u.Pt()
+
+	for y := -ir; y <= ir; y++ {
+		for x := -ir; x <= ir; x++ {
+			if x*x+y*y <= r2 {
+				SetPoint(m, pt.Add(Pt(x, y)), c)
+			}
+		}
+	}
+}
+
+// DrawPointCircle draws a circle at the given point
+func DrawPointCircle(m draw.Image, p image.Point, c color.Color, r int, t float64) {
+	points := circlePoints(p, r)
+
+	switch {
+	case t <= 1:
+		for i := range points {
+			SetPoint(m, points[i], c)
+		}
+	default:
+		center := PV(p)
+
+		for i := range points {
+			from := PV(points[i])
+			to := from.Add(from.To(center).Unit().Scaled(t))
+
+			DrawLine(m, from, to, t, c)
+		}
+	}
+}
+
+func circlePoints(p image.Point, r int) Points {
+	var cp []image.Point
+
+	x, y, dx, dy := r-1, 0, 1, 1
+
+	e := dx - (r << 1)
+
+	for x >= y {
+		cp = append(cp,
+			p.Add(Pt(x, y)),
+			p.Add(Pt(y, x)),
+			p.Add(Pt(-y, x)),
+			p.Add(Pt(-x, y)),
+			p.Add(Pt(-x, -y)),
+			p.Add(Pt(-y, -x)),
+			p.Add(Pt(y, -x)),
+			p.Add(Pt(x, -y)),
+		)
+
+		if e <= 0 {
+			y++
+			e += dy
+			dy += 2
+		}
+
+		if e > 0 {
+			x--
+			dx += 2
+			e += dx - (r << 1)
+		}
+	}
+
+	return cp
+}
+
+// DrawImageRectangle draws a rectangle of the given color on the image.
+func DrawImageRectangle(m draw.Image, r image.Rectangle, c color.Color) {
+	draw.Draw(m, r, &image.Uniform{c}, image.ZP, draw.Over)
+}
+
+// DrawPolygon filled or as line polygons if the thickness is >= 1.
+func DrawPolygon(m draw.Image, p Polygon, c color.Color, t float64) {
+	n := len(p)
+
+	if n < 3 {
+		return
+	}
+
+	switch {
+	case t < 1:
+		p.Fill(m, c)
+	default:
+		for i := 0; i < n; i++ {
+			if i+1 == n {
+				polylineFromTo(p[n-1], p[0], t).Fill(m, c)
+			} else {
+				polylineFromTo(p[i], p[i+1], t).Fill(m, c)
+			}
+		}
+	}
+}
