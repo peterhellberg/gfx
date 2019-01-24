d7861fe84b78153da0cce4728fce88dcaf082713
Peter Hellberg <peter@c7.se>
2019-01-24 16:01:07 +0100

    Add examples for Lerp and Clamp

--- vec_test.go
+++ vec_test.go
@@ -18,3 +18,35 @@ func TestV(t *testing.T) {
 		}
 	}
 }
+
+func ExampleLerp() {
+	a, b := V(1, 2), V(30, 40)
+
+	Dump(
+		Lerp(a, b, 0),
+		Lerp(a, b, 0.1),
+		Lerp(a, b, 0.5),
+		Lerp(a, b, 0.9),
+		Lerp(a, b, 1),
+	)
+
+	// Output:
+	//Vec(1, 2)
+	//Vec(3.9, 5.8)
+	//Vec(15.5, 21)
+	//Vec(27.1, 36.2)
+	//Vec(30, 40)
+}
+
+func ExampleClamp() {
+	Dump(
+		Clamp(-5, 10, 10),
+		Clamp(15, 10, 15),
+		Clamp(25, 10, 20),
+	)
+
+	// Output:
+	// 10
+	// 15
+	// 20
+}
