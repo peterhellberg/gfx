013f21541736d0db40ccf336d175ebb5cfcbbe38
Peter Hellberg <peter@c7.se>
2019-02-01 19:33:06 +0100

    Add ReadFile, ReadJSON, ReadFunc, DecodeJSONFunc

---
+++ file.go
@@ -0,0 +1,33 @@
+package gfx
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+// ReadFile opens a file and calls the given ReadFunc.
+func ReadFile(fn string, rf ReadFunc) error {
+	f, err := os.Open(fn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return rf(f)
+}
+
+// ReadJSON opens and decodes a JSON file.
+func ReadJSON(fn string, v interface{}) error {
+	return ReadFile(fn, DecodeJSONFunc(v))
+}
+
+// ReadFunc is a func that takes a io.Reader and returns an error.
+type ReadFunc func(r io.Reader) error
+
+// DecodeJSONFunc returns a function that takes a reader, and decodes into the given value.
+func DecodeJSONFunc(v interface{}) ReadFunc {
+	return func(r io.Reader) error {
+		return json.NewDecoder(r).Decode(v)
+	}
+}
