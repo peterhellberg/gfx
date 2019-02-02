5357e74278d78a93ed4e13a8d4e344aeffa75b6a
Peter Hellberg <peter@c7.se>
2019-02-02 17:46:52 +0100

    Add Pi, CmplxSin and CmplxPhase

--- math.go
+++ math.go
@@ -1,6 +1,14 @@
 package gfx
 
-import "math"
+import (
+	"math"
+	"math/cmplx"
+)
+
+// Mathematical constants.
+var (
+	Pi = 3.14159265358979323846264338327950288419716939937510582097494459 // https://oeis.org/A000796
+)
 
 // MathMin returns the smaller of x or y.
 func MathMin(x, y float64) float64 {
@@ -30,6 +38,17 @@ func MathSin(x float64) float64 {
 // MathCos returns the cosine of the radian argument x.
 func MathCos(x float64) float64 {
 	return math.Cos(x)
+}
+
+// CmplxSin returns the sine of x.
+func CmplxSin(x complex128) complex128 {
+	return cmplx.Sin(x)
+}
+
+// CmplxPhase returns the phase (also called the argument) of x.
+// The returned value is in the range [-Pi, Pi].
+func CmplxPhase(x complex128) float64 {
+	return cmplx.Phase(x)
 }
 
 // Sign returns -1 for values < 0, 0 for 0, and 1 for values > 0.
