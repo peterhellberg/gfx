e5abac99bcec57ce1b26e1e2415280a70a996a00
Peter Hellberg <peter@c7.se>
2019-02-02 17:47:43 +0100

    Add ComplexAt and At

--- palette.go
+++ palette.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"math"
 	"math/rand"
 )
 
@@ -95,6 +96,61 @@ func (p Palette) AsColorPalette() color.Palette {
 	}
 
 	return cp
+}
+
+// ComplexAt returns the color at the given complex128 value.
+func (p Palette) ComplexAt(z complex128) color.Color {
+	t := CmplxPhase(z)/Pi + 1
+
+	if t > 1 {
+		t = 2 - t
+	}
+
+	return p.At(t)
+}
+
+// At returns the color at the given float64 value (range 0-1)
+func (p Palette) At(t float64) color.Color {
+	n := len(p)
+	if t <= 0 || math.IsNaN(t) {
+		return p[0]
+	}
+	if t >= 1 {
+		return p[n-1]
+	}
+
+	i := int(math.Floor(t * float64(n-1)))
+	s := 1 / float64(n-1)
+
+	return lerpColors(p[i], p[i+1], (t-float64(i)*s)/s)
+}
+
+func lerpColors(c0, c1 color.Color, t float64) color.Color {
+	if t <= 0 {
+		return c0
+	}
+
+	if t >= 1 {
+		return c1
+	}
+
+	r0, g0, b0, a0 := c0.RGBA()
+	r1, g1, b1, a1 := c1.RGBA()
+
+	fr0, fg0, fb0, fa0 := float64(r0), float64(g0), float64(b0), float64(a0)
+	fr1, fg1, fb1, fa1 := float64(r1), float64(g1), float64(b1), float64(a1)
+
+	fr := Clamp(fr0+(fr1-fr0)*t, 0, 0xffff)
+	fg := Clamp(fg0+(fg1-fg0)*t, 0, 0xffff)
+	fb := Clamp(fb0+(fb1-fb0)*t, 0, 0xffff)
+	fa := Clamp(fa0+(fa1-fa0)*t, 0, 0xffff)
+
+	r := uint16(fr)
+	g := uint16(fg)
+	b := uint16(fb)
+	a := uint16(fa)
+
+	return color.RGBA64{r, g, b, a}
 }
 
 // sqDiff returns the squared-difference of x and y, shifted by 2 so that
