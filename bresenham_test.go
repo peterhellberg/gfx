018c2812ada74d0899bfce6be7106055d98a7106
Peter Hellberg <peter@c7.se>
2019-02-05 10:52:26 +0100

    Add example of steep Bresenham line

--- bresenham_test.go
+++ bresenham_test.go
@@ -25,3 +25,29 @@ func ExampleDrawBresenhamLine() {
 	//░░░░░░░░░░
 	//
 }
+
+func ExampleDrawBresenhamLine_steep() {
+	dst := NewPaletted(10, 5, Palette1Bit, ColorWhite)
+
+	DrawBresenhamLine(dst, V(7, 3), V(6, 1), ColorBlack)
+
+	for y := 0; y < dst.Bounds().Dy(); y++ {
+		for x := 0; x < dst.Bounds().Dx(); x++ {
+			if dst.Index(x, y) == 0 {
+				Printf("▓")
+			} else {
+				Printf("░")
+			}
+		}
+		Printf("\n")
+	}
+
+	// Output:
+	//
+	//░░░░░░░░░░
+	//░░░░░░▓░░░
+	//░░░░░░▓░░░
+	//░░░░░░░▓░░
+	//░░░░░░░░░░
+	//
+}
