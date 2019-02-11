be0bca768fe746f25163d12a3714972e88a0f332
Peter Hellberg <peter@c7.se>
2019-02-11 10:59:51 +0100

    Add gfx.SortSlice

---
+++ sort.go
@@ -0,0 +1,8 @@
+package gfx
+
+import "sort"
+
+// SortSlice sorts the provided slice given the provided less function.
+func SortSlice(slice interface{}, less func(i, j int) bool) {
+	sort.Slice(slice, less)
+}
