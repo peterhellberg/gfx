f5b3161598db1dc87f5edec712b20ee1569e35d2
Peter Hellberg <peter@c7.se>
2019-02-11 04:38:15 +0100

    Increase size of triangles example image

--- examples/gfx-example-triangles/gfx-example-triangles.go
+++ examples/gfx-example-triangles/gfx-example-triangles.go
@@ -6,13 +6,13 @@ var p = gfx.PaletteFamicube
 
 func main() {
 	n := 50
-	m := gfx.NewPaletted(800, 270, p, p.Color(n+7))
+	m := gfx.NewPaletted(900, 270, p, p.Color(n+7))
 	t := gfx.NewDrawTarget(m)
 
 	t.MakeTriangles(&gfx.TrianglesData{
-		vx(64, 16, n+1), vx(6, 142, n+2), vx(302, 142, n+3),
-		vx(300, 142, n+4), vx(450, 50, n+5), vx(590, 236, n+6),
-		vx(550, 70, n+8), vx(770, 150, n+9), vx(620, 236, n+10),
+		vx(114, 16, n+1), vx(56, 142, n+2), vx(352, 142, n+3),
+		vx(350, 142, n+4), vx(500, 50, n+5), vx(640, 236, n+6),
+		vx(640, 70, n+8), vx(820, 160, n+9), vx(670, 236, n+10),
 	}).Draw()
 
 	gfx.SavePNG("gfx-example-triangles.png", m)
