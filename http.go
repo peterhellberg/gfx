2d4240f7763692332b1a9ff705a4a6458ff2056e
Peter Hellberg <peter@c7.se>
2019-02-15 20:19:24 +0100

    Add GetImage

--- http.go
+++ http.go
@@ -6,14 +6,30 @@ import (
 	"time"
 )
 
-// DefaultClient is the default HTTP client used by the gfx package.
-var DefaultClient = &http.Client{
-	Timeout: 30 * time.Second,
+// HTTP is the HTTP client and user agent used by the gfx package.
+type HTTP struct {
+	*http.Client
+	UserAgent string
+}
+
+// HTTPClient is the default client used by Get/GetPNG/GetTileset, etc.
+var HTTPClient = HTTP{
+	Client: &http.Client{
+		Timeout: 30 * time.Second,
+	},
+	UserAgent: "gfx.HTTPClient",
 }
 
 // Get performs a HTTP GET request using the DefaultClient.
 func Get(rawurl string) (*http.Response, error) {
-	return DefaultClient.Get(rawurl)
+	req, err := http.NewRequest(http.MethodGet, rawurl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", HTTPClient.UserAgent)
+
+	return HTTPClient.Do(req)
 }
 
 // GetPNG retrieves a remote PNG using DefaultClient
@@ -25,6 +41,17 @@ func GetPNG(rawurl string) (image.Image, error) {
 	defer resp.Body.Close()
 
 	return DecodePNG(resp.Body)
+}
+
+// GetImage retrieves a remote image using DefaultClient
+func GetImage(rawurl string) (image.Image, error) {
+	resp, err := Get(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return DecodeImage(resp.Body)
 }
 
 // GetTileset retrieves a remote tileset using GetPNG.
