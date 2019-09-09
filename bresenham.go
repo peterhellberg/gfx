af463ec4f5dcb89818c61ab97a12dbe1fdec9dda
Peter Hellberg <peter@c7.se>
2019-09-09 07:47:33 +0200

    Use math.Abs instead of inlined abs function

--- bresenham.go
+++ bresenham.go
@@ -3,6 +3,7 @@ package gfx
 import (
 	"image/color"
 	"image/draw"
+	"math"
 )
 
 // DrawLineBresenham draws a line using Bresenham's line algorithm.
@@ -12,15 +13,7 @@ func DrawLineBresenham(dst draw.Image, from, to Vec, c color.Color) {
 	x0, y0 := from.XY()
 	x1, y1 := to.XY()
 
-	abs := func(i float64) float64 {
-		if i < 0 {
-			return -i
-		}
-
-		return i
-	}
-
-	steep := abs(y0-y1) > abs(x0-x1)
+	steep := math.Abs(y0-y1) > math.Abs(x0-x1)
 
 	if steep {
 		x0, y0 = y0, x0
@@ -33,7 +26,7 @@ func DrawLineBresenham(dst draw.Image, from, to Vec, c color.Color) {
 	}
 
 	dx := x1 - x0
-	dy := abs(y1 - y0)
+	dy := math.Abs(y1 - y0)
 	e := dx / 2
 	y := y0
 
