e5bdd7e9774b39208a2e7713125bfed6e4931f74
Peter Hellberg <peter@c7.se>
2020-04-13 17:39:36 +0200

    Draw triangle using all three corner colors

--- triangle.go
+++ triangle.go
@@ -120,11 +120,20 @@ func (t Triangle) EachPixel(tf TriangleFunc) {
 	}
 }
 
-// Draw the first color in the triangle to dst.
+// Draw the triangle to dst.
 func (t Triangle) Draw(dst draw.Image) (drawCount int) {
-	a, _, _ := t.Colors()
+	b := t.Bounds()
 
-	return t.DrawColor(dst, a)
+	for x := b.Min.X; x < b.Max.X; x++ {
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			if u := IV(x, y); t.Contains(u) {
+				drawCount++
+				SetVec(dst, u, t.Color(u))
+			}
+		}
+	}
+
+	return drawCount
 }
 
 // DrawOver draws the first color in the triangle over dst.
