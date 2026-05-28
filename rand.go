4fb35cd3e2131c3f62244584c1d688d69ecba92e
Peter Hellberg <peter@c7.se>
2026-05-28 18:12:23 +0200

    rand: Replaced the deprecated rand.Seed call with a package-level *rand.Rand

--- rand.go
+++ rand.go
@@ -1,24 +1,37 @@
-//go:build !tinygo
-// +build !tinygo
-
 package gfx
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
-// RandSeed uses the provided seed value to initialize the default Source to a
-// deterministic state. If Seed is not called, the generator behaves as
-// if seeded by Seed(1). Seed values that have the same remainder when
-// divided by 2^31-1 generate the same pseudo-random sequence.
-// RandSeed, unlike the Rand.Seed method, is safe for concurrent use.
+var (
+	randMu  sync.Mutex
+	randGen = rand.New(rand.NewSource(1))
+)
+
+// RandSeed uses the provided seed value to initialize the gfx random number
+// generator to a deterministic state. If RandSeed is not called, the
+// generator behaves as if seeded by RandSeed(1).
+//
+// RandSeed is safe for concurrent use.
 func RandSeed(seed int64) {
-	rand.Seed(seed)
+	randMu.Lock()
+	randGen.Seed(seed)
+	randMu.Unlock()
 }
 
-// RandIntn returns, as an int, a non-negative pseudo-random number in [0,n)
-// from the default Source.
+// RandIntn returns, as an int, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
-func RandIntn(n int) int { return rand.Intn(n) }
+func RandIntn(n int) int {
+	randMu.Lock()
+	defer randMu.Unlock()
+	return randGen.Intn(n)
+}
 
-// RandFloat64 returns, as a float64, a pseudo-random number in [0.0,1.0)
-// from the default Source.
-func RandFloat64() float64 { return rand.Float64() }
+// RandFloat64 returns, as a float64, a pseudo-random number in [0.0,1.0).
+func RandFloat64() float64 {
+	randMu.Lock()
+	defer randMu.Unlock()
+	return randGen.Float64()
+}
